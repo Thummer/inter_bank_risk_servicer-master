7f7b2e2d38b91dd26741062ca9cfaca4f5ab183c
Summer <summer2645@hotmail.com>
2019-08-06 18:12:38 +0800

    sample

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,13 +2,7 @@
 
     <el-row style="margin-bottom: 70px; margin-top: 70px">
         <hr/>
-        <el-col :span="5"><el-link type="info">Collection</el-link></el-col>
-        <el-col :span="1"><div style="width: 2px; background: #2c3e50">'</div></el-col>
-        <el-col :span="5">  <el-link type="info">Site</el-link></el-col>
-        <el-col :span="1"><div style="width: 3px; background: #2c3e50">'</div></el-col>
-        <el-col :span="5">  <el-link type="info">Shop</el-link></el-col>
-        <el-col :span="1"><div style="width: 3px; background: #2c3e50">'</div></el-col>
-        <el-col :span="5">  <el-link type="info">About</el-link></el-col>
+    <p style="color: dimgray">Copyright @2019 by 某科学的花旗炮 项目组</p>
     </el-row>
 
 </template>

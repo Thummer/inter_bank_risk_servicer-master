234975fbc3b4ab6dc6c27f1315391883a7552ed5
Summer <summer2645@hotmail.com>
2019-09-11 22:18:24 +0800

    完善法律条款

--- src/components/ApplyLoan.vue
+++ src/components/ApplyLoan.vue
@@ -8,6 +8,27 @@
             <el-breadcrumb-item>申请贷款</el-breadcrumb-item>
         </el-breadcrumb>
 
+        <div>
+            <el-dialog
+                    title="提示"
+                    :visible.sync="dialogVisible"
+                    width="30%"
+                    :before-close="handleClose">
+                <p><h3>法律声明</h3>
+                法律声明
+                1.任何单位和个人不得强令商业银行发放贷款或者提供担保。商业银行有权拒绝任何单位和个人强令要求其发放贷款或者提供担保。——《中华人民共和国商业银行法》 <br>
+                2.借款人应当按期归还贷款的本金和利息。借款人到期不归还担保贷款的，商业银行依法享有要求保证人归还贷款本金和利息或者就该担保物优先受偿的权利。商业银行因行使抵押权、质权而取得的不动产或者股权，应当自取得之日起二年内予以处分。借款人到期不归还信用贷款的，应当按照合同约定承担责任。——《中华人民共和国商业银行法》<br>
+                3.有下列情形之一的大额贷款，鼓励采取银团贷款方式：（一）大型集团客户、大型项目融资和大额流动资金融资；（二）单一企业或单一项目融资总额超过贷款行资本净额10%的；（三）单一集团客户授信总额超过贷款行资本净额15%的；（四）借款人以竞争性谈判选择银行业金融机构进行项目融资的。各地银行业协会可以根据以上原则，结合本地区实际情况，组织辖内会员银行共同确定银团贷款额度的具体下限。——《银团贷款业务指引》<br>
+                4.银团贷款由借款人或银行发起。牵头行应当与借款人谈妥银团贷款的初步条件，并获得借款人签署的银团贷款委任书。——《银团贷款业务指引》<br>
+                5.银团贷款收费是指银团成员接受借款人委托，为借款人提供银团筹组、包销安排、贷款承诺、银团事务管理等服务而收取的相关中间业务费用，纳入商业银行中间业务收费管理。银团贷款收费应当按照“自愿协商、公平合理、质价相符”的原则由银团成员和借款人协商确定，并在银团贷款合同或费用函中载明。——《银团贷款业务指引》<br>
+                </p>
+                <span slot="footer" class="dialog-footer">
+    <el-button @click="cancel">拒 绝</el-button>
+    <el-button type="primary" @click="confirm">同 意</el-button>
+  </span>
+            </el-dialog>
+        </div>
+
         <h1>申请贷款</h1>
         <el-row>
             <el-col style="margin: 50px 250px" :span="16">
@@ -67,6 +88,7 @@
         },
         data() {
             return {
+                dialogVisible:'true',
                 ruleForm: {
                     proID: '',
                     borrowlimit: '',
@@ -130,7 +152,21 @@
             },
             resetForm(formName) {
                 this.$refs[formName].resetFields();
-            }
+            },
+            cancel(){
+                this.dialogVisible = false;
+                this.$router.push({path: '/services/loan/'});
+            },
+            confirm(){
+                this.dialogVisible = false;
+            },
+            handleClose(done) {
+                this.$confirm('确认关闭？')
+                    .then(_ => {
+                        this.$router.push({path: '/services/loan/'});
+                    })
+                    .catch(_ => {});
+            },
         }
     }
 </script>
